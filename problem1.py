fc84e352b8db6aede6ec7f22ea5d38116589874a
Janakiraman B <janakiraman171201@gmail.com>
2025-07-14 01:38:51 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,20 @@
+# write a program to show such type of layout of number and square :
+
+
+print("Number \t \t Square")
+print("1 \t\t" + str(1*1))
+print("2 \t\t" + str(2*2))
+print("3 \t\t" + str(3*3))
+print("4 \t\t" + str(4*4))
+
+
+
+
+# write a python program to show such type of layout number of number, square and cube
+
+print("Number \t\t  Sqaure \t\t Cube")
+print("1 \t\t"+str(1*1)+"\t\t\t" + str(1*1*1))
+print("1 \t\t"+str(2*2)+"\t\t\t" + str(2*2*2))
+print("1 \t\t"+str(3*3)+"\t\t\t" + str(3*3*3))
+print("1 \t\t"+str(4*4)+"\t\t\t" + str(4*4*4))
+print("1 \t\t"+str(5*5)+"\t\t\t" + str(5*5*5))
