57dad29704022cb7c4460e5e1d2b476d2caace0c
Janakiraman B <janakiraman171201@gmail.com>
2025-07-15 02:19:44 +0530

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,15 @@
+# write a program to get name and age from the user and display name and age on the screen format : Hi jana! Your age is 23
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+print(f"Hi {name }! Your age is {age}")
+
+
+
+# write a program to get marks and address from the user and display name and age on the screen format : your address is ".." Your marks is "..."
+
+
+address = input("Enter your address: ")
+city = input("Enter your city: ")
+print(f"Your address is {address}" +" "+"and"+" "+ f"your marks is {city}")
