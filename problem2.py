6842316cb04fffca00e997b138dea039c5682dec
Janakiraman B <janakiraman171201@gmail.com>
2025-07-15 02:19:23 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,18 @@
+# Write a program to calculate the area of circle using formula ₶r2 "\u03c0" = pi symbol
+
+print("\u03c0")
+
+radius = float(input("Enter a circle radius: "))
+area = 3.14*radius*radius
+
+print(f"The circle of the radius {area}\u03c0")
+
+
+# wite a python program to calculate the area of rectangle using formula (A*B)
+
+a = float(input("Enter your width: "))
+b = float(input("Enter your length: "))
+
+rectangle = a*b
+
+print(f"The area of Rectangle :{rectangle}cm²")
