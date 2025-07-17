3a0f77b36c649cde6b059f1faa58106b46f4fe69
Janakiraman B <janakiraman171201@gmail.com>
2025-07-18 01:06:35 +0530

    Add files via upload

---
+++ problem5.py
@@ -0,0 +1,42 @@
+# Write a program that performs All Arithmetic operations on Two variables
+
+#Arithmetic operators
+
+a = 5
+b = 3
+
+c=a+b   #addition
+c1=a-b  #subtraction
+c2=a*b  #multiple  
+c3=a/b  #division
+c4=a%b  #exponential
+c5=a**b #square
+
+print(f"1.Addition (a + b) = {c}")
+print(f"2.Subtraction (a - b) = {c1}")
+print(f"3.multiplication (a * b) = {c2}")
+print(f"4.Division (a / b) = {c3}")
+print(f"5.Exponential (a % b)= {c4}")
+print(f"6.Square (a ** b)= {c5}")
+
+
+# Write a python program that perform all arithmetic operations on three variables :
+
+a = 1
+b = 2
+c=  5
+
+c=a+b+c   #addition
+c1=a-b-c  #subtraction
+c2=a*b*c  #multiple  
+c3=a/b/c  #division
+c4=a%b%c  #exponential
+c5=a**b**c #square
+
+
+print(f"1.Addition (a + b + c) = {c}")
+print(f"2.Subtraction (a - b - c) = {c1}")
+print(f"3.multiplication (a * b * c) = {c2}")
+print(f"4.Division (a / b / c) = {c3}")
+print(f"5.Exponential (a % b % )= {c4}")
+print(f"6.Square (a ** b ** c)= {c5}")
