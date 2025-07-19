365e2835e5e583d3178f237694a9b2770c808aad
Janakiraman B <janakiraman171201@gmail.com>
2025-07-20 00:30:34 +0530

    Add files via upload

---
+++ problem8.py
@@ -0,0 +1,17 @@
+# write a program to calculate Intersection of two set :
+
+a = {5,2,4,6,7,1}
+b = {5,3,2,11}
+
+c = a.intersection(b)
+
+print(c)
+
+# Write a python program to calculate intersection of three set :
+a = {5,2,4,6,7,1}
+b = {5,3,11}
+c = {4,5,12,2,1,0}
+
+d = a.intersection(b,c)
+
+print(d)
