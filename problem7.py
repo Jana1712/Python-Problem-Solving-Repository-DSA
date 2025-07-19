cd4222526f8c92e7569969be08b9c0ced93c48c6
Janakiraman B <janakiraman171201@gmail.com>
2025-07-20 00:29:48 +0530

    Add files via upload

---
+++ problem7.py
@@ -0,0 +1,19 @@
+#Write a program to calculate union of two set:
+
+a = {3,2,4,5,6,7,8}
+b = {4,12,5,1,6,8}
+c = {13,2,3,14,6,15}
+d = a.union(b,c)
+
+print(d)
+
+
+# write a program to calculate union of three set
+
+a = {5,12,52,0,8}
+b = {2,5,1,9,8}
+c = {4,5,6,0,10}
+
+d = a.union(b,c)
+
+print(d)
