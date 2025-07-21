fd45c14ac362f7329a7d19703ba11058628efb82
Janakiraman B <janakiraman171201@gmail.com>
2025-07-22 03:16:09 +0530

    Add files via upload

---
+++ Problem9.py
@@ -0,0 +1,17 @@
+#Write a program to calculate difference of two set:
+
+a = {1,12,2,6,7,8}
+b = {15,0,1,3,6}
+
+c = a.difference(b)
+
+print(c)
+
+
+# write a program to calculate symmetric difference of two sets:
+
+a = {12,2,0,3,8}
+b = {15,10,12,3,6}
+c = a.symmetric_difference(b)
+
+print(c)
